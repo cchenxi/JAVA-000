8276a49c64e897990e311a2ddfec236d5386438d
chenxi <chenxi.et@gmail.com>
2020-11-26 21:19:16 +0800

    week-6 第6周 电商表设计

--- Week_06/src/main/resources/homework.sql
+++ Week_06/src/main/resources/homework.sql
@@ -145,9 +145,21 @@ create table t_sku_store_house (
 ) default charset utf8mb4 comment 'SKU_仓库表';
 
 -- 4. 订单相关表
--- TODO
-
--- 4.1. 订单表
-
--- 4.2. 订单商品表
-
+create table t_order (
+    `pk_id` bigint(20) unsigned not null default 0 primary key comment '主键',
+    `order_no` varchar(200) not null default '' comment '订单编号',
+    `order_status` varchar(100) not null default '' comment '订单状态',
+    `receiver_name` varchar(200) not null default '' comment '收件人姓名',
+    `receiver_telephone` varchar(200) not null default '' comment '收件人电话',
+    `receiver_address` varchar(200) not null default '' comment '收件人地址',
+    `receiver_postcode` varchar(10) not null default '' comment '收件人邮编',
+    `sku_id` bigint(20) unsigned not null default 0 comment 'SKU id',
+    `sku_price` decimal(10, 2) unsigned not null default 0 comment 'SKU价格',
+    `order_time` datetime not null default current_timestamp comment '下单时间',
+    `pay_time` datetime not null default current_timestamp comment '支付时间',
+    `dispatch_time` datetime not null default current_timestamp comment '发货时间',
+    `receive_time` datetime not null default current_timestamp comment '收货时间',
+    `create_time` datetime not null default current_timestamp comment '创建时间',
+    `update_time` datetime not null default current_timestamp on update current_timestamp comment '修改时间',
+    `is_deleted` tinyint(2) not null default 0 comment '逻辑删除标识，未删除(0)，已删除(1)'
+) default charset utf8mb4 comment '订单表';
