e368d4a9fc3198246cce076251dbc39ba2575aeb
chenxi <chenxi.et@gmail.com>
2020-12-08 21:33:36 +0800

    week-8 init homework repo

---
+++ Week_08/distributed-tx-demo/src/main/java/io/github/cchenxi/tx/DistributedTxDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.cchenxi.tx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTxDemoApplication.class, args);
+    }
+
+}
---
+++ Week_08/distributed-tx-demo/src/test/java/io/github/cchenxi/tx/DistributedTxDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.cchenxi.tx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class DistributedTxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
